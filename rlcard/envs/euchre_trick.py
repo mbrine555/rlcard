545ddd128b84f279c80b09287097e266cbd2fb02
mbrine555 <m.brine555@gmail.com>
2020-05-10 19:45:11 -0400

    Euchre env

---
+++ rlcard/envs/euchre_trick.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from rlcard.envs.env import Env
+from rlcard.games.euchre_trick.game import EuchreGame as Game
+from rlcard.games.euchre_trick.utils import (ACTION_LIST, ACTION_SPACE,
+                                             CARD_IDX, TRUMP_IDX)
+
+
+class EuchreEnv(Env):
+
+    def __init__(self, config):
+        self.game = Game()
+        super().__init__(config)
+        # State is concatenated boolean vectors of hand, cards in
+        # the center, and trump
+        self.state_shape = [len(CARD_IDX)*2+4]
+
+    def _extract_state(self, state):
+        state['legal_actions'] = self._get_legal_actions()
+        state['raw_legal_actions'] = self.game.get_legal_actions()
+ 
+        hand = [CARD_IDX[card] for card in state['hand']]
+        state_rep = np.zeros(self.state_shape)
+        state_rep[hand] = 1
+        
+        if len(state['center']) != 0:
+            center = [CARD_IDX[card]+len(CARD_IDX) for card in state['center'].values()]
+            state_rep[center] = 1
+        if state['trump'] is not None:
+            state_rep[len(CARD_IDX)*2+TRUMP_IDX[state['trump']]] = 1
+
+        state['obs'] = state_rep
+        return state
+
+    def _decode_action(self, action_id):
+        return ACTION_LIST[action_id]
+
+    def _get_legal_actions(self):
+        legal_actions = self.game.get_legal_actions()
+        legal_ids = [ACTION_SPACE[action] for action in legal_actions]
+        return legal_ids
+
+    def get_payoffs(self):
+        return self.game.get_payoffs()
--- rlcard/games/euchre_trick/utils.py
+++ rlcard/games/euchre_trick/utils.py
@@ -6,6 +6,103 @@ LEFT = {'D': 'HJ', 'H': 'DJ', 'C': 'SJ', 'S': 'CJ'}
 # Order of cards from low->high for non-trump suits
 NON_TRUMP_ORDER = ['9', 'T', 'J', 'Q', 'K', 'A']
 
+# Card to index
+CARD_IDX = {
+    'HA': 0,
+    'HK': 1,
+    'HQ': 2,
+    'HJ': 3,
+    'HT': 4,
+    'H9': 5,
+    'DA': 6,
+    'DK': 7,
+    'DQ': 8,
+    'DJ': 9,
+    'DT': 10,
+    'D9': 11,
+    'SA': 12,
+    'SK': 13,
+    'SQ': 14,
+    'SJ': 15,
+    'ST': 16,
+    'S9': 17,
+    'CA': 18,
+    'CK': 19,
+    'CQ': 20,
+    'CJ': 21,
+    'CT': 22,
+    'C9': 23
+}
+
+# Trump index
+TRUMP_IDX = {
+    'H': 0,
+    'D': 1,
+    'C': 2,
+    'S': 3
+}
+
+# Action to index
+ACTION_SPACE = {
+    'pass': 0,
+    'pick': 1,
+    'call-H': 2,
+    'call-D': 3,
+    'call-S': 4,
+    'call-C': 5,
+    'HA': 6,
+    'HK': 7,
+    'HQ': 8,
+    'HJ': 9,
+    'HT': 10,
+    'H9': 11,
+    'DA': 12,
+    'DK': 13,
+    'DQ': 14,
+    'DJ': 15,
+    'DT': 16,
+    'D9': 17,
+    'SA': 18,
+    'SK': 19,
+    'SQ': 20,
+    'SJ': 21,
+    'ST': 22,
+    'S9': 23,
+    'CA': 24,
+    'CK': 25,
+    'CQ': 26,
+    'CJ': 27,
+    'CT': 28,
+    'C9': 29,
+    'discard-HA': 30,
+    'discard-HK': 31,
+    'discard-HQ': 32,
+    'discard-HJ': 33,
+    'discard-HT': 34,
+    'discard-H9': 35,
+    'discard-DA': 36,
+    'discard-DK': 37,
+    'discard-DQ': 38,
+    'discard-DJ': 39,
+    'discard-DT': 40,
+    'discard-D9': 41,
+    'discard-SA': 42,
+    'discard-SK': 43,
+    'discard-SQ': 44,
+    'discard-SJ': 45,
+    'discard-ST': 46,
+    'discard-S9': 47,
+    'discard-CA': 48,
+    'discard-CK': 49,
+    'discard-CQ': 50,
+    'discard-CJ': 51,
+    'discard-CT': 52,
+    'discard-C9': 53
+}
+
+# List of actions
+ACTION_LIST = list(ACTION_SPACE.keys())
+
 def init_euchre_deck():
     ''' Initialize a 24 card Euchre deck
     Returns:
