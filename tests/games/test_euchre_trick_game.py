ed0807ce72952db98f53a34da1066a6b0dc005d0
mbrine555 <m.brine555@gmail.com>
2020-05-10 10:23:30 -0400

    Add in dealer/player test

--- tests/games/test_euchre_trick_game.py
+++ tests/games/test_euchre_trick_game.py
@@ -1,17 +1,25 @@
 import unittest
+import numpy as np
 
 from rlcard.games.euchre_trick.dealer import EuchreDealer
+from rlcard.games.euchre_trick.player import EuchrePlayer as Player
 
 class TestEuchreTrickGame(unittest.TestCase):
 
     def test_euchre_dealer(self):
-        dealer = EuchreDealer()
+        dealer = EuchreDealer(np.random.RandomState())
         self.assertEqual(len(dealer.deck), 24)
 
     def test_euchre_dealer_flip(self):
-        dealer = EuchreDealer()
+        dealer = EuchreDealer(np.random.RandomState())
         actual_top_card = dealer.deck[0]
         self.assertEqual(actual_top_card, dealer.flip_top_card())
+
+    def test_euchre_deal(self):
+        dealer = EuchreDealer(np.random.RandomState())
+        player = Player(player_id=1, np_random=np.random.RandomState())
+        dealer.deal_cards(player, num=5)
+        self.assertEqual(len(player.hand), 5)
 
 if __name__ == '__main__':
     unittest.main()
