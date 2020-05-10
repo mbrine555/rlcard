25ebcc61261e1b20ed5cf1f61e731168af3e3b51
mbrine555 <m.brine555@gmail.com>
2020-05-10 10:36:14 -0400

    Add in various euchre utils

--- rlcard/games/euchre_trick/utils.py
+++ rlcard/games/euchre_trick/utils.py
@@ -1,5 +1,8 @@
 from rlcard.core import Card
 
+# Maps trump suit to left bower
+LEFT = {'D': 'HJ', 'H': 'DJ', 'C': 'SJ', 'S': 'CJ'}
+
 def init_euchre_deck():
     ''' Initialize a 24 card Euchre deck
     Returns:
@@ -9,3 +12,12 @@ def init_euchre_deck():
     rank_list = ['A', '9', 'T', 'J', 'Q', 'K']
     res = [Card(suit, rank) for suit in suit_list for rank in rank_list]
     return res
+
+def cards2list(cards):
+    return [card.get_index() for card in cards]
+
+def is_left(card, trump):
+    return card.get_index() == LEFT[trump]
+
+def is_right(card, trump):
+    return card.get_index() == trump + 'J'
